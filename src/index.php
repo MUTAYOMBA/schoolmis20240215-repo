13fa79f58b80117f4533959eb5cabde4fb7317c8
mutayomba <mutayombajeandedieu@gmail.com>
2024-02-15 15:25:35 +0200

    commiting
    adde to bootsrapp

---
+++ src/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>How To Create Simple Login Form Design In Bootstrap 5</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">    
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://www.markuptag.com/bootstrap/5/css/bootstrap.min.css">
+</head>
+<?php
+echo"<h3> we are dealing with PHP</h3>";
+?>
+<body>
+</head>
+</html>
